fcb4371799c728888ce160a1c5c64f99de8b2543
Z003U7PY <Z003U7PY@AAEINBLR03662D>
2017-10-17 15:37:34 +0530

    SourceCode uploaded

---
+++ BowlingScore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScoreCaculator;
+
+namespace BowlingScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BowlingScorecaculator s = new BowlingScorecaculator();
+            int[] score= new int[20];
+            Console.WriteLine("enter the no of games");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("enter the scores in "+(i+1)+" game");
+                int firstChancehits = Convert.ToInt32(Console.ReadLine());
+                int secondChancehits = Convert.ToInt32(Console.ReadLine());
+                int thirdChancehits = Convert.ToInt32(Console.ReadLine());
+                score[i] = s.finalscore(firstChancehits, secondChancehits, thirdChancehits);
+            }
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("totalscore in "+(i+1)+" game is "+score[i]);
+            }
+        }
+           
+    }
+}
+
---
+++ ScoreCaculator/Scorecaculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreCaculator
+{
+    public class BowlingScorecaculator
+    {
+        int scorein(int Chancehits,int score)
+        {
+            if (Chancehits > 0 && Chancehits < 7)
+                score += 1;
+            else if (Chancehits > 6 && Chancehits < 9)
+                score += 2;
+            return score;
+        }
+        public int finalscore(int firstChancehits, int secondChancehits, int thirdChancehits)
+        {
+            int score = 0;
+
+            if (firstChancehits <= 9 && secondChancehits <= 9 && thirdChancehits <= 9)
+            {
+                if (firstChancehits == 9)
+                {
+                    score += 10;
+                }
+                else if (firstChancehits != 9 && secondChancehits == 9)
+                {
+                    score += 7+ scorein(firstChancehits, score);
+                }
+                else if (firstChancehits != 9 && secondChancehits != 9 && thirdChancehits == 9)
+                {
+                    score += 4+ scorein(firstChancehits, score)+ scorein(secondChancehits, score);
+                }
+                else
+                {
+                  score+=  scorein(firstChancehits,score)+scorein(secondChancehits, score)+scorein(thirdChancehits, score);
+                }
+            }
+            return score;
+        }
+
+    }
+}
